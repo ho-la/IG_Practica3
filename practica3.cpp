49a8088e125ff43cf2f357412cfe236e9a1a5203
ho-la <qaz111wsx222@gmail.com>
2016-11-14 16:01:52 +0100

    iniciando, a ver si sale todos los ficheros

---
+++ practica3.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   practica3.cpp
+ * Author: odopaon
+ *
+ * Created on 14 de noviembre de 2016, 15:58
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
